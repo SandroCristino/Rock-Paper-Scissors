4086ede35cf2b8bc29f06709eff13872703e0d78
SandyWezzy <sandro-cristino@hotmail.de>
2022-10-08 18:29:37 +0200

    build scorebar, restart

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 let start = document.querySelector('#start');
+let box = document.querySelector('#box');
+let rockBtn = document.querySelector('.r');
+const paperBtn = document.querySelector('.p');
+const scissorsBtn = document.querySelector('.s');
+let gameStatus = document.querySelector('#results');
 
+let computerChoice;
 let playerSelection;
-let comScore = 0;
-let playerScore = 0;
 let scoreCounter = 0;
+
+
 
 function getComputerChoice () {
     let result;
@@ -21,75 +27,58 @@ function getComputerChoice () {
 
 function play(playerSelection) {
     let result;
-    let computerChoice = getComputerChoice();
-    let playerChoice = playerSelection.toLowerCase();
+    computerChoice = getComputerChoice();
 
     switch (computerChoice) {
         case 'rock':
-            if (playerChoice == "paper") {
+            if (playerSelection == "paper") {
                 result = 'win';
-            } else if (playerChoice == 'rock') {
+                scoreCounter++;
+                gameStatus.textContent = 'Point for you. Paper beats Rock!';
+            } else if (playerSelection == 'rock') {
                 result = 'draw';
-            } else if (playerChoice == 'scissors') {
+                gameStatus.textContent = 'Ahhh a draw.';
+            } else if (playerSelection == 'scissors') {
                 result = 'lose';
+                scoreCounter--;
+                gameStatus.textContent = 'Daimn. Rock beats Scissors!';
             }
             break;
         case 'paper':
-            if (playerChoice == "paper") {
+            if (playerSelection == "paper") {
                 result = 'draw';
-            } else if (playerChoice == 'rock') {
+                gameStatus.textContent = 'Unfortunaly a drawww.';
+            } else if (playerSelection == 'rock') {
                 result = 'lose';
-            } else if (playerChoice == 'scissors') {
+                scoreCounter--;
+                gameStatus.textContent = 'You lost. Paper beats Rock!';
+            } else if (playerSelection == 'scissors') {
                 result = 'win';
+                scoreCounter++;
+                gameStatus.textContent = 'Nice. Scissors beats Paper!';
             }
             break;
         case 'scissors':
-            if (playerChoice == "paper") {
+            if (playerSelection == "paper") {
                 result = 'lose';
-            } else if (playerChoice == 'rock') {
+                scoreCounter--;
+                gameStatus.textContent = 'Noop. Scissors beats Paper!';
+            } else if (playerSelection == 'rock') {
                 result = 'win';
-            } else if (playerChoice == 'scissors') {
+                scoreCounter++;
+                gameStatus.textContent = 'Good Job. Rock beats Scissors!';
+            } else if (playerSelection == 'scissors') {
                 result = 'draw';
+                gameStatus.textContent = 'How bad. A Draw.'
             }
             break;
         default:
             result = "undefined";
     }
-    return result;
-}
-
-function game() {
-    let result;
-    let check = 0;
-    for (i=1;i<=5;i++) {
-        while (check == 0) {
-        playerSelection = prompt("Paper, Scissors or Rock");
-        playerSelection = playerSelection.toLowerCase();
-        if (playerSelection == 'paper' || playerSelection == 'scissors' || playerSelection == 'rock') {
-            check++;
-        } else {
-            alert('Word have nothing to do with Paper, Scissors or Rock. Please choose correct');
-        }
-        }
-
-        result = play(playerSelection);
-        if (result == 'lose') {
-            comScore++;
-            console.log('You lose');
-        } else if (result == 'win') {
-            playerScore++;
-            console.log('You win');
-        } else {
-            console.log('Its a draw');
-        }
-        check--;
-    }
-    result = (comScore > playerScore) ? 
-        'Computer WINS' : (comScore < playerScore) ?
-        'Player WINS' : 'Its a DRAW';
-
+    updateBar();
+    endResult();
     
-    return result;
+
 }
 
 function updateBar() {
@@ -97,41 +86,73 @@ function updateBar() {
     case 0:
       comBar.style.width = '50%';
       playerBar.style.width = '50%';
+      break;
     case 1:
       comBar.style.width = '40%';
       playerBar.style.width = '60%';
+      break;
     case 2:
       comBar.style.width = '25%';
       playerBar.style.width = '75%';
+      break;
     case 3:
       comBar.style.width = '0%';
       playerBar.style.width = '100%';
+      break;
     case -1:
       comBar.style.width = '60%';
-      playerBar.style.width = '40%';c
+      playerBar.style.width = '40%';
+      break;
     case -2:
       comBar.style.width = '75%';
       playerBar.style.width = '25%';
+      break;
     case -3: 
       comBar.style.width = '100%';
       playerBar.style.width = '0%';
+      break;
   }
 }
 
+
 function endResult() {
-  if (scoreCounter == -4) {
+  if (scoreCounter == -3) {
+    updateEndScene();
+    gameStatus.textContent = 'YOU LOST';
     defeated.style.display = 'flex';
     defeated.style.animation = 'slideIn 2s';
-  } else {
+  } else if (scoreCounter == 3) {
+    updateEndScene();
+    gameStatus.textContent = 'YOU WIN';
     winner.style.display = 'flex';
     winner.style.animation = 'slideInR  2s';
   }
 }
 
+function updateEndScene() {
+  container.style.display = 'none';
+  barContainer.style.display = 'none';
+  main.style.height = '250px';
+  start.textContent = 'Restart';
+  start.style.display = 'block';
+}
+
 start.addEventListener('click',() => {
-    start.style.display = 'none';
-    container.style.display = 'flex';
+  winner.style.display = 'none';
+  defeated.style.display = 'none';
+  container.style.display = 'flex'
+  barContainer.style.display = 'flex';
+  start.style.display = 'none';
+  scoreCounter = 0;
+  gameStatus.textContent = '';
+  comBar.style.width = '50%';
+  playerBar.style.width = '50%';
   })
+
+rockBtn.addEventListener('click', () => play('rock'));
+paperBtn.addEventListener('click', () => play('paper'));
+scissorsBtn.addEventListener('click', () => play('scissors'));
+
 
 
 
